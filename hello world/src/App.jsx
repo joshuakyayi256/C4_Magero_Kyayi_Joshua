f827b0367d4338ad211e6f5ee482cf6c2f45c02c
Magero Joshua <kyayijoshua@gmail.com>
2023-11-08 10:56:31 +0300

    first commit

---
+++ hello world/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>I am MAGERO KYAYI JOSHUA, this is my first React application</p>
+    </div>
+  )
+}
+
+export default App
