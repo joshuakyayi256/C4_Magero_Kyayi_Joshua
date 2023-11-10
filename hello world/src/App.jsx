3dbe1766f09294800018876c99686968450dec85
Magero Joshua <kyayijoshua@gmail.com>
2023-11-10 22:06:31 +0300

    second commit

--- hello world/src/App.jsx
+++ hello world/src/App.jsx
@@ -1,17 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React from 'react';
+
+const user = {
+  fullName: 'MAGERO KYAYI JOSHUA',
+  education: 'MAKERERE UNIVERSITY AND AZUBI AFRICA',
+  professionalBackground: 'STUDENT',
+  skills: ['UI DESIGN', 'FRONT-END DEVELOPMENT', 'CLOUD ENGINEERING'],
+};
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h1>Hello World</h1>
-      <p>I am MAGERO KYAYI JOSHUA, this is my first React application</p>
+    <div className="portfolio">
+      <h1>{user.fullName}</h1>
+      <section>
+        <h2>Education</h2>
+        <p>{user.education}</p>
+      </section>
+      <section>
+        <h2>Professional Background</h2>
+        <p>{user.professionalBackground}</p>
+      </section>
+      <section>
+        <h2>Skills</h2>
+        <ul>
+          {user.skills.map((skill, index) => (
+            <li key={index}>{skill}</li>
+          ))}
+        </ul>
+      </section>
     </div>
-  )
+  );
 }
 
 export default App
+
+
+
